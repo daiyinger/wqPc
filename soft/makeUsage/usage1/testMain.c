9675745b65c77ed284db4925736d41af75ea258d
daiyinger <1002639334@qq.com>
2015-11-10 14:11:56 +0800

    add a readme file

---
+++ soft/makeUsage/usage1/testMain.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "func1.h"
+#include "func2.h"
+int main(void)
+{
+	printf("OK now!\n");
+	func1();
+	func2();
+	return 0;
+}
---
+++ soft/makeUsage/usage2/func2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int func2(void)
+{
+	printf("func 2 Called!\n");
+	return 0;
+}
